f5c3fb3f517366fbf22968806f8d3cbc6a7e6c89
ikopylov <ikopylov@users.noreply.github.com>
2015-12-01 15:52:46 +0300

    String distance calculation updated. Unit tests added

---
+++ src/Qoollo.Turbo.UnitTests/Common/StringDistanceTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qoollo.Turbo.UnitTests.Common
+{
+    [TestClass]
+    public class StringDistanceTest
+    {
+        [TestMethod]
+        public void HammingStringDistance()
+        {
+            var dist1 = StringDistance.GetHammingDistance("abcd", "accd");
+            Assert.AreEqual(1, dist1);
+        }
+
+        [TestMethod]
+        public void LevenshteinStringDistance()
+        {
+            var dist1 = StringDistance.GetLevenshteinDistance("Test string one", "Test string one");
+            Assert.AreEqual(0, dist1);
+
+            var dist2 = StringDistance.GetLevenshteinDistance("Test string one", "string one");
+            Assert.AreEqual(5, dist2);
+
+            var dist3 = StringDistance.GetLevenshteinDistance("Test string one", "Tesd");
+            Assert.AreEqual(12, dist3);
+
+            var dist4 = StringDistance.GetLevenshteinDistance("String 1 test", "Absolutly different val");
+            Assert.AreEqual(20, dist4);
+
+            var dist5 = StringDistance.GetLevenshteinDistance("141154342", "141,154,342");
+            Assert.AreEqual(2, dist5);
+
+            var dist6 = StringDistance.GetLevenshteinDistance("-141154342", "(141,154,342)");
+            Assert.AreEqual(4, dist6);
+        }
+
+        [TestMethod]
+        public void DamerauLevenshteinStringDistance()
+        {
+            var dist1 = StringDistance.GetLevenshteinDistance("string", "strnig", true);
+            Assert.AreEqual(1, dist1);
+        }
+    }
+}
--- src/Qoollo.Turbo/Common/StringDistance.cs
+++ src/Qoollo.Turbo/Common/StringDistance.cs
@@ -36,6 +36,70 @@ namespace Qoollo.Turbo
         }
 
 
+
+        /// <summary>
+        /// Calculates the Levenshtein distance between two strings
+        /// </summary>
+        /// <param name="strA">First string</param>
+        /// <param name="strB">Second string</param>
+        /// <param name="withTranspositions">Take symbol transposition into account (Damerau-Levenshtein distance)</param>
+        /// <returns>Levenshtein distance</returns>
+        public static int GetLevenshteinDistance(string strA, string strB, bool withTranspositions)
+        {
+            Contract.Requires<ArgumentNullException>(strA != null);
+            Contract.Requires<ArgumentNullException>(strB != null);
+
+            int boundHeight = strA.Length + 1;
+            int boundWidth = strB.Length + 1;
+
+            int[] prevPrevLine = withTranspositions ? new int[boundWidth] : null;
+            int[] previousLine = new int[boundWidth];
+            int[] currentLine = new int[boundWidth];
+            int[] tmpLineSwap = null;
+
+            for (int width = 0; width < boundWidth; width++)
+                currentLine[width] = width;
+
+            for (int height = 1; height < boundHeight; height++)
+            {
+                if (withTranspositions)
+                {
+                    tmpLineSwap = prevPrevLine;
+                    prevPrevLine = previousLine;
+                    previousLine = currentLine;
+                    currentLine = tmpLineSwap;
+                }
+                else
+                {
+                    tmpLineSwap = previousLine;
+                    previousLine = currentLine;
+                    currentLine = tmpLineSwap;
+                }
+
+                currentLine[0] = height;
+
+                for (int width = 1; width < boundWidth; width++)
+                {
+                    int cost = (strA[height - 1] == strB[width - 1]) ? 0 : 1;
+                    int insertion = currentLine[width - 1] + 1;
+                    int deletion = previousLine[width] + 1;
+                    int substitution = previousLine[width - 1] + cost;
+
+                    int distance = Math.Min(insertion, Math.Min(deletion, substitution));
+
+                    if (withTranspositions && height > 1 && width > 1 && strA[height - 1] == strB[width - 2] && strA[height - 2] == strB[width - 1])
+                    {
+                        distance = Math.Min(distance, prevPrevLine[width - 2] + cost);
+                    }
+
+                    currentLine[width] = distance;
+                }
+            }
+
+            return currentLine[boundWidth - 1];
+        }
+
+
         /// <summary>
         /// Calculates the Levenshtein distance between two strings
         /// </summary>
@@ -44,80 +108,7 @@ namespace Qoollo.Turbo
         /// <returns>Levenshtein distance</returns>
         public static int GetLevenshteinDistance(string strA, string strB)
         {
-            Contract.Requires<ArgumentNullException>(strA != null);
-            Contract.Requires<ArgumentNullException>(strB != null);
-
-            int boundHeight = strA.Length + 1;
-            int boundWidth = strB.Length + 1;
-
-            int[,] matrix = new int[boundHeight, boundWidth];
-
-            for (int height = 0; height < boundHeight; height++) 
-                matrix[height, 0] = height; 
-            for (int width = 0; width < boundWidth; width++)
-                matrix[0, width] = width; 
-
-            for (int height = 1; height < boundHeight; height++)
-            {
-                for (int width = 1; width < boundWidth; width++)
-                {
-                    int cost = (strA[height - 1] == strB[width - 1]) ? 0 : 1;
-                    int insertion = matrix[height, width - 1] + 1;
-                    int deletion = matrix[height - 1, width] + 1;
-                    int substitution = matrix[height - 1, width - 1] + cost;
-
-                    int distance = Math.Min(insertion, Math.Min(deletion, substitution));
-
-                    matrix[height, width] = distance;
-                }
-            }
-
-            return matrix[boundHeight - 1, boundWidth - 1];
-        }
-
-
-        /// <summary>
-        /// Calculates the Damerau-Levenshtein distance between two strings
-        /// </summary>
-        /// <param name="strA">First string</param>
-        /// <param name="strB">Second string</param>
-        /// <returns>Damerau-Levenshtein distance</returns>
-        public static int GetDamerauLevenshteinDistance(string strA, string strB)
-        {
-            Contract.Requires<ArgumentNullException>(strA != null);
-            Contract.Requires<ArgumentNullException>(strB != null);
-
-            int boundHeight = strA.Length + 1;
-            int boundWidth = strB.Length + 1;
-
-            int[,] matrix = new int[boundHeight, boundWidth];
-
-            for (int height = 0; height < boundHeight; height++)
-                matrix[height, 0] = height;
-            for (int width = 0; width < boundWidth; width++)
-                matrix[0, width] = width;
-
-            for (int height = 1; height < boundHeight; height++)
-            {
-                for (int width = 1; width < boundWidth; width++)
-                {
-                    int cost = (strA[height - 1] == strB[width - 1]) ? 0 : 1;
-                    int insertion = matrix[height, width - 1] + 1;
-                    int deletion = matrix[height - 1, width] + 1;
-                    int substitution = matrix[height - 1, width - 1] + cost;
-
-                    int distance = Math.Min(insertion, Math.Min(deletion, substitution));
-
-                    if (height > 1 && width > 1 && strA[height - 1] == strB[width - 2] && strA[height - 2] == strB[width - 1])
-                    {
-                        distance = Math.Min(distance, matrix[height - 2, width - 2] + cost);
-                    }
-
-                    matrix[height, width] = distance;
-                }
-            }
-
-            return matrix[boundHeight - 1, boundWidth - 1];
+            return GetLevenshteinDistance(strA, strB, false);
         }
     }
 }
