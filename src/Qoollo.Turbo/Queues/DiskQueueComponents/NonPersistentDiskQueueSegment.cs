d16d844b56f1b551f7cdb9042800bc5e04052c3c
ikopylov <ikopylov@users.noreply.github.com>
2017-04-11 22:45:14 +0300

    NonPersistenDisqkQueueSegment

---
+++ src/Qoollo.Turbo/Queues/DiskQueueComponents/NonPersistentDiskQueueSegment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qoollo.Turbo.Queues.DiskQueueComponents
+{
+    class NonPersistentDiskQueueSegment<T> : CountingDiskQueueSegment<T>
+    {
+        public NonPersistentDiskQueueSegment(int capacity, long segmentNumber)
+            : base(capacity, 0, 0, segmentNumber)
+        {
+
+        }
+
+
+        protected override void AddCore(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TryTakeCore(out T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TryPeekCore(out T item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override void Dispose(DiskQueueSegmentDisposeBehaviour disposeBehaviour, bool isUserCall)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
