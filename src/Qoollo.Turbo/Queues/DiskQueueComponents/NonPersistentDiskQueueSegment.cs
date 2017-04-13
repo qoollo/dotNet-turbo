2f9d113594483377c5caf9c99a1aab1c1a52eb96
ikopylov <ikopylov@users.noreply.github.com>
2017-04-13 22:43:37 +0300

    Non persistent DiskQueueSegment started

--- src/Qoollo.Turbo/Queues/DiskQueueComponents/NonPersistentDiskQueueSegment.cs
+++ src/Qoollo.Turbo/Queues/DiskQueueComponents/NonPersistentDiskQueueSegment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,21 @@ namespace Qoollo.Turbo.Queues.DiskQueueComponents
 {
     class NonPersistentDiskQueueSegment<T> : CountingDiskQueueSegment<T>
     {
-        public NonPersistentDiskQueueSegment(int capacity, long segmentNumber)
+        private readonly string _fileName;
+        private readonly FileStream _writeStream;
+        private readonly FileStream _readStream;
+
+        public NonPersistentDiskQueueSegment(string fileName, int capacity, long segmentNumber)
             : base(capacity, 0, 0, segmentNumber)
         {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+            if (File.Exists(fileName))
+                throw new ArgumentException($"Can't create NonPersistentDiskQueueSegment on existing file '{fileName}'", nameof(fileName));
 
+            _fileName = fileName;
+            _writeStream = new FileStream(_fileName, FileMode.CreateNew, FileAccess.Write, FileShare.Read);
+            _readStream = new FileStream(_fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
         }
 
 
