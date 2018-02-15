6fd5d5d03602dc108f32b0e37199bc349eb52da0
ikopylov <ikopylov@users.noreply.github.com>
2018-02-15 22:48:09 +0300

    Additional tests for reflection based methods

---
+++ src/Qoollo.Turbo.UnitTests/Threading/ExecutionContextHelperTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Qoollo.Turbo.Threading.ServiceStuff;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Qoollo.Turbo.UnitTests.Threading
+{
+    [TestClass]
+    public class ExecutionContextHelperTest
+    {
+        class CustomSyncContext : SynchronizationContext
+        {
+        }
+
+        [TestMethod]
+        public void CaptureContextNoSyncContextTest()
+        {
+            var originalContext = SynchronizationContext.Current;
+            var syncContext = new CustomSyncContext();
+            try
+            {
+                SynchronizationContext.SetSynchronizationContext(syncContext);
+                var eContext = ExecutionContextHelper.CaptureContextNoSyncContext();
+                Assert.IsNotNull(eContext);
+
+                bool isDefaulContext = false;
+                ExecutionContext.Run(eContext, (st) =>
+                {
+                    isDefaulContext = SynchronizationContext.Current == null;
+                }, null);
+
+                Assert.IsTrue(isDefaulContext, "Default context expected");
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(originalContext);
+            }
+        }
+    }
+}
