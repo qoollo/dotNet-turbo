360ccfa141b392b70d0536d05946762395872b13
ikopylov <ikopylov@users.noreply.github.com>
2020-09-07 22:48:10 +0300

    Initial SpinWait normalization implemented

---
+++ src/Qoollo.Turbo.UnitTests/Threading/SpinWaitHelperTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Qoollo.Turbo.Threading.ServiceStuff;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Qoollo.Turbo.UnitTests.Threading
+{
+    [TestClass]
+    public class SpinWaitHelperTest: TestClassBase
+    {
+        [TestMethod]
+        public void SpinWaitTimeValidationTest()
+        {
+            for (int i = 0; i < 30000; i++)
+                Thread.SpinWait(1000);
+        }
+
+#if NETCOREAPP3_1
+        [TestMethod]
+        public void SpinWaitOnNetCore31IsAlwaysNormalized()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int normValue = SpinWaitHelper.MeasureSpinWaitNormalizationCoef();
+                Assert.AreEqual(1, normValue);
+                Thread.Sleep(1);
+            }
+        }
+#endif
+
+        [TestMethod]
+        public void SpinWaitSmallFluctuation()
+        {
+            List<int> measureResults = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                measureResults.Add(SpinWaitHelper.MeasureSpinWaitNormalizationCoef());
+                Thread.Sleep(1);
+            }
+
+            int minMeasure = measureResults.Min();
+            int maxMeasure = measureResults.Max();
+            Assert.IsTrue(maxMeasure - minMeasure <= 1);
+        }
+
+
+        [TestMethod]
+        [Timeout(5000)]
+        public void SpinWaitPerformNormalizationInBackground()
+        {
+            while (!SpinWaitHelper.NormalizationCoefCalculated)
+            {
+                SpinWaitHelper.SpinWait(100);
+            }
+        }
+
+
+        private void SpinningNormalizedValidationCore()
+        {
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < 500; i++)
+                SpinWaitHelper.SpinWait(37 * 1000);
+            sw.Stop();
+            TestContext.WriteLine($"Measured time: {sw.ElapsedMilliseconds}ms");
+            Assert.IsTrue(sw.ElapsedMilliseconds > 400 && sw.ElapsedMilliseconds < 800, "Measured time: " + sw.ElapsedMilliseconds.ToString());
+        }
+
+        [TestMethod]
+        [Timeout(5000)]
+        public void SpinningNormalizedValidation()
+        {
+            while (!SpinWaitHelper.NormalizationCoefCalculated)
+            {
+                SpinWaitHelper.SpinWait(100);
+            }
+
+            SpinningNormalizedValidationCore();
+            SpinningNormalizedValidationCore();
+            SpinningNormalizedValidationCore();
+        }
+    }
+}
--- src/Qoollo.Turbo/Threading/ServiceStuff/SpinWaitHelper.cs
+++ src/Qoollo.Turbo/Threading/ServiceStuff/SpinWaitHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,18 +11,31 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
 {
     internal static class SpinWaitHelper
     {
+        private const int NORM_COEF_NOT_CALCULATED = 0;
+        private const int NORM_COEF_CALC_STARTED = 1;
+        private const int NORM_COEF_CALC_FINISHED = 2;
+
         private const int NsPerSecond = 1000 * 1000 * 1000;
         /// <summary>
         /// Coefficient getted from Net Core runtime as the base for normalization (this should result in the same behavior as in .NET Core 3.0 runtime)
         /// </summary>
         private const int MinNsPerNormalizedSpin = 37;
 
+
         private static int _normalizationCoef = 1;
-        private static volatile bool _normalizationCalculated = false;
+        private static volatile int _normalizationCalculated = NORM_COEF_NOT_CALCULATED;
 
-        public static int NormalizationCoef { get { return _normalizationCoef; } }
+        public static int NormalizationCoef
+        {
+            get
+            {
+                EnsureSpinWaitNormalizationCoefCalculated();
+                return _normalizationCoef;
+            }
+        }
+        internal static bool NormalizationCoefCalculated { get { return _normalizationCalculated == NORM_COEF_CALC_FINISHED; } }
 
-        internal static double MeasureSpinWaitNormalizationCoef()
+        internal static double MeasureSpinWaitNormalizationCoefSinglePass()
         {
             int spinCount = 0;
             long expectedDuration = 20 * Stopwatch.Frequency / 1000; // 20ms
@@ -37,13 +51,61 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
 
 
             double nsPerSpin = (double)elapsedTime * NsPerSecond / ((double)spinCount * Stopwatch.Frequency);
-            return nsPerSpin / MinNsPerNormalizedSpin;
+            return MinNsPerNormalizedSpin / nsPerSpin;
+        }
+
+        internal static int MeasureSpinWaitNormalizationCoef()
+        {
+            double mainMeasure = MeasureSpinWaitNormalizationCoefSinglePass();
+
+            // Validate it is within expected range
+            if (mainMeasure < 0.9 || mainMeasure > 1.1)
+            {
+                // Suspicies result: repeat measurements
+                double[] allMeasures = new double[3];
+                allMeasures[0] = mainMeasure;
+                allMeasures[1] = MeasureSpinWaitNormalizationCoefSinglePass();
+                allMeasures[2] = MeasureSpinWaitNormalizationCoefSinglePass();
+                Array.Sort(allMeasures);
+                mainMeasure = allMeasures[1];
+            }
+
+            if (mainMeasure < 1)
+                mainMeasure = 1;
+            else if (mainMeasure > 32)
+                mainMeasure = 32;
+
+            return (int)Math.Round(mainMeasure);
+        }
+
+
+        private static void MeasureSpinWaitNormalizationCoefThreadFunc(object state)
+        {
+            Interlocked.Exchange(ref _normalizationCoef, MeasureSpinWaitNormalizationCoef());
+            Interlocked.Exchange(ref _normalizationCalculated, NORM_COEF_CALC_FINISHED);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void EnsureSpinWaitNormalizationCoefCalculatedSlow()
+        {
+            if (Interlocked.CompareExchange(ref _normalizationCalculated, NORM_COEF_CALC_STARTED, NORM_COEF_NOT_CALCULATED) == NORM_COEF_NOT_CALCULATED)
+            {
+                ThreadPool.QueueUserWorkItem(MeasureSpinWaitNormalizationCoefThreadFunc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void EnsureSpinWaitNormalizationCoefCalculated()
+        {
+            if (_normalizationCalculated == NORM_COEF_NOT_CALCULATED)
+                EnsureSpinWaitNormalizationCoefCalculatedSlow();
         }
 
 
         public static void SpinWait(int iterations)
         {
-            Thread.SpinWait(iterations);
+            EnsureSpinWaitNormalizationCoefCalculated();
+            Thread.SpinWait(iterations * _normalizationCoef);
         }
     }
 }
