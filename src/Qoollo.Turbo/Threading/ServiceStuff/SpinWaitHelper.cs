03cc403de5394ad5defef548e9a0d42b68c71536
ikopylov <ikopylov@users.noreply.github.com>
2020-09-23 22:43:06 +0300

    SpinWaitHelper comments

--- src/Qoollo.Turbo/Threading/ServiceStuff/SpinWaitHelper.cs
+++ src/Qoollo.Turbo/Threading/ServiceStuff/SpinWaitHelper.cs
@@ -9,12 +9,27 @@ using System.Threading.Tasks;
 
 namespace Qoollo.Turbo.Threading.ServiceStuff
 {
+    /// <summary>
+    /// SpinWait with normalization. Performs period normalization to the post-skylake level. Should be roughly the same as .NET Core 3.1 logic
+    /// </summary>
     internal static class SpinWaitHelper
     {
+        /// <summary>
+        /// CPU kind
+        /// </summary>
         internal enum ProcessorKind
         {
+            /// <summary>
+            /// Other
+            /// </summary>
             Other = 0,
+            /// <summary>
+            /// Intel before Skylake. Expected normalization coefficient: 8-10
+            /// </summary>
             IntelPreSkylake,
+            /// <summary>
+            /// Intel Skylake and later. Expected normalization coefficient: 1
+            /// </summary>
             IntelPostSkylake
         }
 
@@ -36,7 +51,9 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
 
 
 #if NETFRAMEWORK || NETSTANDARD
-
+        /// <summary>
+        /// SpinWait normalization coefficient. Number of iterations passed to <see cref="SpinWait(int)"/> will be multiplied to this value
+        /// </summary>
         public static int NormalizationCoef
         {
             get
@@ -45,18 +62,34 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
                 return _normalizationCoef;
             }
         }
+        /// <summary>
+        /// True when <see cref="NormalizationCoef"/> is calculated, otherwise False
+        /// </summary>
         internal static bool NormalizationCoefCalculated { get { return _normalizationCalculated == NORM_COEF_CALC_FINISHED; } }
 
+        /// <summary>
+        /// Causes a thread to wait the number of times defined by the iterations parameter (normalized version)
+        /// </summary>
+        /// <param name="iterations">Integer that defines how long a thread is to wait</param>
         public static void SpinWait(int iterations)
         {
             EnsureSpinWaitNormalizationCoefCalculated();
             Thread.SpinWait(iterations * _normalizationCoef);
         }
 #else
-
+        /// <summary>
+        /// SpinWait normalization coefficient. Number of iterations passed to <see cref="SpinWait(int)"/> will be multiplied to this value
+        /// </summary>
         public static int NormalizationCoef { get { return 1; } }
+        /// <summary>
+        /// True when <see cref="NormalizationCoef"/> is calculated, otherwise False
+        /// </summary>
         internal static bool NormalizationCoefCalculated { get { return true; } }
 
+        /// <summary>
+        /// Causes a thread to wait the number of times defined by the iterations parameter (normalized version)
+        /// </summary>
+        /// <param name="iterations">Integer that defines how long a thread is to wait</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void SpinWait(int iterations)
         {
@@ -64,7 +97,9 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
         }
 #endif
 
-
+        /// <summary>
+        /// Triggers calculation of <see cref="NormalizationCoef"/> and waits for its completion
+        /// </summary>
         internal static void WaitUntilNormalizationCoefCalculated()
         {
             EnsureSpinWaitNormalizationCoefCalculated();
@@ -80,7 +115,13 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
             }
         }
 
-
+        /// <summary>
+        /// Attempts to parse CPU identifier part
+        /// </summary>
+        /// <param name="parts">CPU identifier splitted by words</param>
+        /// <param name="partName">Name of the part (example: 'Family', 'Model')</param>
+        /// <param name="value">Parsed value of the part</param>
+        /// <returns>True if parsed successfully</returns>
         private static bool TryParseProcessorIdentifierPart(string[] parts, string partName, out int value)
         {
             value = 0;
@@ -90,6 +131,10 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
 
             return int.TryParse(parts[partIndex + 1], out value);
         }
+        /// <summary>
+        /// Gets CPU kind. Uses 'PROCESSOR_IDENTIFIER' environment variable, so valid only on Windows
+        /// </summary>
+        /// <returns>CPU kind</returns>
         internal static ProcessorKind GetProcessorKind()
         {
             var processorIdentifier = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
@@ -116,13 +161,21 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
             return ProcessorKind.IntelPostSkylake;
         }
 
+        /// <summary>
+        /// Checkes whether NET Framework supports SpinWait normalization out of the box
+        /// </summary>
+        /// <returns>True if supports (for .NET Core 3.0 and later), otherwise False</returns>
         internal static bool IsFrameworkSupportSpinWaitNormalization()
         {
             var hiddenField = typeof(Thread).GetProperty("OptimalMaxSpinWaitsPerSpinIteration", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
             return hiddenField != null;
         }
 
-
+        /// <summary>
+        /// Performes single mesure of normalization coefficient
+        /// </summary>
+        /// <param name="measureDurationMs">Duration of the measure in milliseconds</param>
+        /// <returns></returns>
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static double MeasureSpinWaitNormalizationCoef(int measureDurationMs)
         {
@@ -143,6 +196,10 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
             return MinNsPerNormalizedSpin / nsPerSpin;
         }
 
+        /// <summary>
+        /// Calculates <see cref="NormalizationCoef"/> based on several measures, on running Framework and etc.
+        /// </summary>
+        /// <returns>Calculated normalization coefficient</returns>
         internal static int GetSpinWaitNormalizationCoef()
         {
             // Check whether framework support normalization out of the box (for NETSTANDARD)
@@ -156,10 +213,10 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
                 // Choose the largest one (larger coef -> larger spinCount in specified interval -> less probability of context switching)
                 mainMeasure = MeasureSpinWaitNormalizationCoef(measureDurationMs: 8);
 
-                Thread.Yield();
-                mainMeasure = Math.Max(mainMeasure, MeasureSpinWaitNormalizationCoef(measureDurationMs: 8));
+                Thread.Sleep(0);
+                mainMeasure = Math.Max(mainMeasure, MeasureSpinWaitNormalizationCoef(measureDurationMs: 4));
 
-                Thread.Yield();
+                Thread.Sleep(0);
                 mainMeasure = Math.Max(mainMeasure, MeasureSpinWaitNormalizationCoef(measureDurationMs: 8));
             }
             else
@@ -183,7 +240,10 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
             return (int)Math.Round(mainMeasure);
         }
 
-
+        /// <summary>
+        /// Method that runs on separate thread and runs Normalization coefficient calculation
+        /// </summary>
+        /// <param name="state">State parameter</param>
         private static void MeasureSpinWaitNormalizationCoefThreadFunc(object state)
         {
             try
@@ -202,6 +262,9 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
             }
         }
 
+        /// <summary>
+        /// Ensures that <see cref="NormalizationCoef"/> calculated and, if not, starts the calculation (slow path)
+        /// </summary>
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static void EnsureSpinWaitNormalizationCoefCalculatedSlow()
         {
@@ -210,7 +273,9 @@ namespace Qoollo.Turbo.Threading.ServiceStuff
                 ThreadPool.QueueUserWorkItem(MeasureSpinWaitNormalizationCoefThreadFunc);
             }
         }
-
+        /// <summary>
+        /// Ensures that <see cref="NormalizationCoef"/> calculated and, if not, starts the calculation
+        /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void EnsureSpinWaitNormalizationCoefCalculated()
         {
